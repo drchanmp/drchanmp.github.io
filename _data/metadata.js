ae7526daede2a18a2b3b00e329d91a7e9311d580
Drchanmp <drcklinn@protonmail.com>
2025-05-13 07:45:19 +0630

    Update _data/metadata.js (via Pages CMS)

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "My Personal Blog",
+	url: "https://drchanmp.github.io/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Personal Blog of Drchanmp, CEO of Mcmpacme, Inc.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Drchanmp",
+		email: "drcklinn@protonmail.com",
+		url: "https://drchanmp.github.io/about-me/"
 	}
 }
